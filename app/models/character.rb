f31d368b83542812f3237c2177a802f52f680af9
daisy oliani <daisy.oliani@gmail.com>
2024-11-20 14:18:19 +0100

    show characters bookings owner side

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,6 +1,6 @@
 class Character < ApplicationRecord
   belongs_to :user
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
 
   validates :name, :gender, :image_url, :price,  presence: true
   validates :description, length: { minimum: 10 }
