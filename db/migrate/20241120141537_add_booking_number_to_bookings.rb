84f611c235bacd6b2fddc75792dfef987426d302
Antoine Bataille van der Elst <bataillevdeantoine@gmail.com>
2024-11-20 17:10:54 +0100

    lamerde

--- db/migrate/20241120141537_add_booking_number_to_bookings.rb
+++
@@ -1,6 +0,0 @@
-class AddBookingNumberToBookings < ActiveRecord::Migration[7.1]
-  def change
-    add_column :bookings, :booking_number, :string
-    add_index :bookings, :booking_number, unique: true
-  end
-end
--- db/migrate/20241120143129_add_booking_number_to_characters.rb
+++
@@ -1,5 +0,0 @@
-class AddBookingNumberToCharacters < ActiveRecord::Migration[7.1]
-  def change
-    add_column :characters, :booking_number, :string
-  end
-end
