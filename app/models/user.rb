d120b9bccce69a935869c4a40c579eb6491959c5
Youssef Saroute <youssef.saroute77@gmail.com>
2024-11-21 16:18:12 +0100

    validates + forms

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,16 @@
 class User < ApplicationRecord
   has_many :characters
   has_many :bookings
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  # validates :first_name, :last_name
+
+  validates :first_name, :last_name, presence: true
+  validates :nickname, presence: true, length: { in: 3..50 }, uniqueness: { case_sensitive: false }
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :password, length: { minimum: 6 }, allow_blank: true
 
   def owns?(character)
     self.characters.pluck(:id).include?(character.id)
